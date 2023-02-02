03d04498a2e9c3fb8affd15715b8d20639f471c5
Osman Coskun <osman.coskun@pardus.org.tr>
2023-02-02 08:59:35 +0300

    fixin lighthouse scores

--- src/App.svelte
+++ src/App.svelte
@@ -9,8 +9,13 @@
     experiences: Experiences,
     projects: Projects,
   };
+  let meta_desc =
+    "Meet Osman Coskun, a skilled software engineer with a passion for code. View his portfolio and read about his experiences in the tech industry. Connect with Osman and be inspired by his journey as a software engineer.";
 </script>
 
+<svelte:head>
+  <meta name="description" content={meta_desc} />
+</svelte:head>
 <main class="max-w-5xl mx-auto p-3 rounded-lg mb-5">
   <Header />
   <div class="rounded-lg md:p-5 md:m-4">
